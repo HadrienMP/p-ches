c63e33427adf21da3932634d1b7cbb0c2faadd92
hadrienmp <public@hadrienmp.fr>
2022-04-14 22:00:23 +0200

    💄 make a circle out of the notes

--- src/Main.elm
+++ src/Main.elm
@@ -113,7 +113,6 @@ subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
         [ Time.every (toFloat <| msBetweenQuarterNotes tempo) Tick
-        , Browser.Events.onClick (Json.Decode.succeed Stab)
         , Browser.Events.onKeyDown (Json.Decode.succeed Stab)
         ]
 
@@ -161,99 +160,135 @@ view model =
         ]
     <|
         column [ centerY, centerX, spacing 60, paddingXY 10 28 ]
-            [ title
-            , row [ centerX, spacingXY 40 0 ]
-                [ button
-                    [ Element.Border.solid
-                    , Element.Border.color white
-                    , Element.Border.rounded 10
-                    , Element.Border.width 2
-                    , padding 10
-                    ]
-                    { onPress =
-                        case model of
-                            On _ ->
-                                Just Stop
-
-                            Off ->
-                                Just Start
-                    , label =
-                        case model of
-                            On _ ->
-                                text "Stop"
-
-                            Off ->
-                                text "Start"
-                    }
-                , text <| "Tempo: " ++ String.fromInt (getBpm tempo)
-                ]
-            , displayNotes model
+            [ displayNotes model
+            , playStopButon model
             ]
+
+
+playStopButon : Model -> Element Msg
+playStopButon model =
+    row [ centerX, spacingXY 40 0 ]
+        [ button
+            [ Element.Border.solid
+            , Element.Border.color white
+            , Element.Border.rounded 10
+            , Element.Border.width 2
+            , padding 10
+            ]
+            { onPress =
+                case model of
+                    On _ ->
+                        Just Stop
+
+                    Off ->
+                        Just Start
+            , label =
+                case model of
+                    On _ ->
+                        text "Stop"
+
+                    Off ->
+                        text "Start"
+            }
+        , text <| "Tempo: " ++ String.fromInt (getBpm tempo)
+        ]
 
 
 displayNotes : Model -> Element Msg
 displayNotes model =
-    row [ spacing 4, width fill, centerX, Element.Font.family [ Element.Font.sansSerif ] ]
-        (notes |> List.map (displayNote model))
+    let
+        jojo =
+            displayNote model
+    in
+    column
+        [ centerX
+        , Element.Font.family [ Element.Font.sansSerif ]
+        , inFront <|
+            button [ centerX, centerY ]
+                { label =
+                    image [ width <| px 100 ]
+                        { src = "/img/peach.png", description = "peach" }
+                , onPress = Just Stab
+                }
+        ]
+        [ row [ centerX, height <| px 22 ]
+            [ jojo [ alignTop ] { sixteenth = 1, label = "1", quarter = True } ]
+        , row [ centerX, spacing 66, height <| px 20 ]
+            [ jojo [] { sixteenth = 16, label = "4", quarter = False }
+            , jojo [] { sixteenth = 2, label = "2", quarter = False }
+            ]
+        , row [ centerX, spacing 138 ]
+            [ jojo [] { sixteenth = 15, label = "3", quarter = False }
+            , jojo [] { sixteenth = 3, label = "3", quarter = False }
+            ]
+        , row [ centerX, spacing 188, height <| px 44 ]
+            [ jojo [] { sixteenth = 14, label = "2", quarter = False }
+            , jojo [] { sixteenth = 4, label = "4", quarter = False }
+            ]
+        , row [ centerX, spacing 194, height <| px 56 ]
+            [ jojo [] { sixteenth = 13, label = "4", quarter = True }
+            , jojo [] { sixteenth = 5, label = "2", quarter = True }
+            ]
+        , row [ centerX, spacing 188, height <| px 44 ]
+            [ jojo [] { sixteenth = 12, label = "4", quarter = False }
+            , jojo [] { sixteenth = 6, label = "2", quarter = False }
+            ]
+        , row [ centerX, spacing 138 ]
+            [ jojo [] { sixteenth = 11, label = "3", quarter = False }
+            , jojo [] { sixteenth = 7, label = "3", quarter = False }
+            ]
+        , row [ centerX, alignTop, spacing 66, height <| px 20 ]
+            [ jojo [] { sixteenth = 10, label = "2", quarter = False }
+            , jojo [] { sixteenth = 8, label = "4", quarter = False }
+            ]
+        , row [ centerX, height <| px 22 ]
+            [ jojo [ alignBottom ] { sixteenth = 9, label = "3", quarter = True } ]
+        ]
 
 
-displayNote : Model -> Note -> Element Msg
-displayNote model note =
+displayNote : Model -> List (Attribute Msg) -> Note -> Element Msg
+displayNote model attributes note =
+    el (attributes ++ commonNoteStyle note ++ currentNoteStyling model note) <|
+        text note.label
+
+
+commonNoteStyle : Note -> List (Attribute msg)
+commonNoteStyle note =
+    [ Element.Font.center
+    , Element.Border.solid
+    , Element.Border.color white
+    , Element.Border.rounded 100
+    , if note.quarter then
+        Element.Border.width 2
+
+      else
+        Element.Border.width 1
+    , if note.quarter then
+        paddingEach { top = 10, left = 13, right = 13, bottom = 8 }
+
+      else
+        paddingXY 10 8
+    , if note.quarter then
+        Element.Font.size 20
+
+      else
+        Element.Font.size 10
+    ]
+
+
+currentNoteStyling : Model -> Note -> List (Attribute Msg)
+currentNoteStyling model note =
     case model of
         On on ->
-            el
-                [ Element.Font.center
-                , Element.Border.solid
-                , Element.Border.color white
-                , Element.Border.rounded 10
-                , if note.quarter then
-                    Element.Border.width 2
+            [ if note.sixteenth == on.sixteenth then
+                Element.Background.color yellow
 
-                  else
-                    Element.Border.width 1
-                , if note.sixteenth == on.sixteenth then
-                    Element.Background.color yellow
-
-                  else
-                    Element.Font.center
-                , if note.quarter then
-                    paddingEach { top = 10, left = 8, right = 8, bottom = 8 }
-
-                  else
-                    padding 8
-                , if note.quarter then
-                    Element.Font.size 20
-
-                  else
-                    Element.Font.size 10
-                ]
-            <|
-                text note.label
+              else
+                Element.Font.center
+            ]
 
         Off ->
-            el
-                [ Element.Font.center
-                , Element.Border.solid
-                , Element.Border.color white
-                , if note.quarter then
-                    Element.Border.width 2
-
-                  else
-                    Element.Border.width 1
-                , Element.Border.rounded 10
-                , if note.quarter then
-                    paddingEach { top = 10, left = 8, right = 8, bottom = 8 }
-
-                  else
-                    padding 8
-                , if note.quarter then
-                    Element.Font.size 20
-
-                  else
-                    Element.Font.size 10
-                ]
-            <|
-                text note.label
+            []
 
 
 title : Element.Element Msg
